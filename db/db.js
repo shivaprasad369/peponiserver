cacb82ee7af72d6019f647912f6b8fb305a8831e
shivaprasad <shivu369sapare@gmail.com>
2025-02-25 09:21:22 +0530

    add

--- db/db.js
+++ db/db.js
@@ -7,9 +7,9 @@ const db = mysql.createPool({
     password: process.env.DB_PASSWORD,           
     // password: '',    
     database: process.env.DB_NAME,
-    waitForConnections: true,
+    // waitForConnections: true,
     // connectionLimit: 10,       // Adjust based on your workload
-    queueLimit: 0,            // No limit on the queue
+    // queueLimit: 0,            // No limit on the queue
     // connectTimeout: 10000,    // 10 seconds
 });
 
