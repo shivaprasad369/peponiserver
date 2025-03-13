68eecdf858a4689d45a5b9982c0359361a0cf5ea
shivaprasad sapare <122955366+shivaprasad369@users.noreply.github.com>
2025-03-13 10:48:51 +0530

    Update user.js

--- User/user.js
+++ User/user.js
@@ -151,7 +151,20 @@ userRoute.get("/firebase/email/:email", async (req, res) => {
     res.status(404).json({ error: "User not found" });
   }
 });
+userRoute.delete("/firebase/email/:email", async (req, res) => {
+  try {
+    // Get user details by email
+    const userRecord = await auth.getUserByEmail(req.params.email);
 
+    // Delete user by UID
+    await auth.deleteUser(userRecord.uid);
+    
+    res.status(200).json({ message: `User with email ${req.params.email} deleted successfully.` });
+  } catch (error) {
+    console.error("Error deleting user:", error.message);
+    res.status(400).json({ error: error.message });
+  }
+});
 
 // Update Profile endpoint
 userRoute.post('/profile', upload.single('profilePicture'), async (req, res) => {
